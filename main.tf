47be9a7eb043d9120c054c13ddae02b6214bbd28
mahesh yadav <mahesh.yadav@clouddrove.com>
2022-12-29 22:14:03 +0530

    update module

--- main.tf
+++ main.tf
@@ -4,14 +4,13 @@
 #Module      : labels
 #Description : Terraform module to create consistent naming for multiple names.
 module "labels" {
-  source = "git::https://github.com/clouddrove/terraform-azure-labels.git?ref=tags/0.12.0"
-
+  source      = "clouddrove/labels/azure"
+  version     = "1.0.0"
   name        = var.name
-  application = var.application
   environment = var.environment
-  tags        = var.tags
   managedby   = var.managedby
   label_order = var.label_order
+  repository  = var.repository
 }
 
 #Module      : NETWORK INTERFACE
@@ -81,6 +80,7 @@ resource "azurerm_public_ip" "default" {
   reverse_fqdn            = var.reverse_fqdn
   public_ip_prefix_id     = var.public_ip_prefix_id
   zones                   = var.zones
+  ddos_protection_mode    = var.ddos_protection_mode
   tags                    = module.labels.tags
 
   timeouts {
@@ -91,119 +91,6 @@ resource "azurerm_public_ip" "default" {
   }
 }
 
-#Module      : STORAGE ACCOUNT
-#Description : Terraform resource to create a storage account for virtual machine.
-resource "azurerm_storage_account" "default" {
-  count                     = var.enabled && var.boot_diagnostics_enabled ? 1 : 0
-  name                      = "teststorageaccount"
-  resource_group_name       = var.resource_group_name
-  location                  = var.location
-  account_kind              = var.account_kind
-  account_tier              = var.account_kind == "BlockBlobStorage" || var.account_kind == "FileStorage" ? "Premium" : var.account_tier
-  account_replication_type  = var.account_replication_type
-  access_tier               = var.account_kind == "BlobStorage" || var.account_kind == "FileStorage" || var.account_kind == "StorageV2" ? var.access_tier : ""
-  enable_https_traffic_only = var.enable_https_traffic_only
-  is_hns_enabled            = var.is_hns_enabled
-  tags                      = module.labels.tags
-
-  dynamic "blob_properties" {
-    for_each = var.blob_properties_enabled ? [1] : []
-
-    content {
-      cors_rule {
-        allowed_headers    = var.allowed_headers
-        allowed_methods    = var.allowed_methods
-        allowed_origins    = var.allowed_origins
-        exposed_headers    = var.exposed_headers
-        max_age_in_seconds = var.max_age_in_seconds
-      }
-
-      delete_retention_policy {
-        days = var.days
-      }
-    }
-  }
-
-  dynamic "custom_domain" {
-    for_each = var.custom_domain_enabled ? [1] : []
-
-    content {
-      name          = format("%s-custom-domain", module.labels.id)
-      use_subdomain = var.use_subdomain
-    }
-  }
-
-  dynamic "identity" {
-    for_each = var.identity_enabled ? [1] : []
-
-    content {
-      type = var.sa_type
-    }
-  }
-
-  dynamic "queue_properties" {
-    for_each = var.queue_properties_enabled ? [1] : []
-
-    content {
-      cors_rule {
-        allowed_headers    = var.allowed_headers
-        allowed_methods    = var.allowed_methods
-        allowed_origins    = var.allowed_origins
-        exposed_headers    = var.exposed_headers
-        max_age_in_seconds = var.max_age_in_seconds
-      }
-
-      logging {
-        delete                = var.log_delete
-        read                  = var.log_read
-        version               = var.log_version
-        write                 = var.log_write
-        retention_policy_days = var.retention_policy_days
-      }
-
-      minute_metrics {
-        enabled               = var.minute_metrics_enabled
-        version               = var.version
-        include_apis          = var.include_apis
-        retention_policy_days = var.retention_policy_days
-      }
-
-      hour_metrics {
-        enabled               = var.hour_metrics_enabled
-        version               = var.version
-        include_apis          = var.include_apis
-        retention_policy_days = var.retention_policy_days
-      }
-    }
-  }
-
-  dynamic "network_rules" {
-    for_each = var.network_rules_enabled ? [1] : []
-
-    content {
-      default_action             = var.default_action
-      bypass                     = [var.bypass]
-      ip_rules                   = azurerm_public_ip.default.*.ip_address
-      virtual_network_subnet_ids = var.subnet_id
-    }
-  }
-
-  dynamic "static_website" {
-    for_each = var.static_website_enabled ? [1] : []
-
-    content {
-      index_document     = var.index_document
-      error_404_document = var.error_404_document
-    }
-  }
-
-  timeouts {
-    create = var.create
-    update = var.update
-    read   = var.read
-    delete = var.delete
-  }
-}
 
 #Module      : LINUX VIRTUAL MACHINE
 #Description : Terraform resource to create a linux virtual machine.
@@ -240,7 +127,7 @@ resource "azurerm_virtual_machine" "default" {
 
     content {
       enabled     = var.boot_diagnostics_enabled
-      storage_uri = join("", azurerm_storage_account.default.*.primary_blob_endpoint)
+      storage_uri = var.blob_endpoint
     }
   }
 
@@ -344,167 +231,6 @@ resource "azurerm_virtual_machine" "default" {
   }
 }
 
-#Module      : VIRTUAL MACHINE
-#Description : Terraform resource to create a virtual machine.
-resource "azurerm_virtual_machine" "default" {
-  count                            = var.enabled ? var.machine_count : 0
-  name                             = format("%s-virtual-machine-%s", module.labels.id, count.index + 1)
-  resource_group_name              = var.resource_group_name
-  location                         = var.location
-  network_interface_ids            = [element(azurerm_network_interface.default.*.id, count.index)]
-  vm_size                          = var.vm_size
-  availability_set_id              = join("", azurerm_availability_set.default.*.id)
-  delete_os_disk_on_termination    = var.delete_os_disk_on_termination
-  delete_data_disks_on_termination = var.delete_data_disks_on_termination
-  license_type                     = var.license_type
-  primary_network_interface_id     = element(azurerm_network_interface.default.*.id, count.index)
-  proximity_placement_group_id     = var.proximity_placement_group_id
-  zones                            = var.zones
-  tags                             = module.labels.tags
-
-  dynamic "os_profile_linux_config" {
-    for_each = var.linux_enabled && var.disable_password_authentication ? [1] : []
-
-    content {
-      disable_password_authentication = var.disable_password_authentication
-
-      ssh_keys {
-        key_data = file(var.file_path)
-        path     = "/home/${var.username}/.ssh/authorized_keys"
-      }
-    }
-  }
-
-  dynamic "os_profile_windows_config" {
-    for_each = var.windows_enabled ? [1] : []
-
-    content {
-      provision_vm_agent        = var.provision_vm_agent
-      enable_automatic_upgrades = var.enable_automatic_upgrades
-      timezone                  = var.timezone
-      
-      winrm {
-        protocol        = var.protocol
-        certificate_url = var.certificate_url
-      }
-
-      additional_unattend_config {
-        pass         = var.pass
-        component    = var.component
-        setting_name = var.setting_name
-        content      = var.content
-      }
-    }
-  }
-
-  dynamic "boot_diagnostics" {
-    for_each = var.boot_diagnostics_enabled ? [1] : []
-
-    content {
-      enabled     = var.boot_diagnostics_enabled
-      storage_uri = join("", azurerm_storage_account.default.*.primary_blob_endpoint)
-    }
-  }
-
-  dynamic "additional_capabilities" {
-    for_each = var.addtional_capabilities_enabled ? [1] : []
-
-    content {
-      ultra_ssd_enabled = var.ultra_ssd_enabled
-    }
-  }
-
-  dynamic "identity" {
-    for_each = var.identity_enabled ? [1] : []
-
-    content {
-      type         = var.vm_type
-      identity_ids = var.identity_ids
-    }
-  }
-
-  dynamic "os_profile" {
-    for_each = var.os_profile_enabled && var.create_option == "FromImage" ? [1] : []
-
-    content {
-      computer_name  = format("%s-virtual-machine-%s", module.labels.id, count.index + 1)
-      admin_username = var.admin_username
-      admin_password = var.windows_enabled && var.disable_password_authentication == false ? var.admin_password : ""
-      custom_data    = file("${path.module}/user-data.tpl")
-    }
-  }
-
-  dynamic "os_profile_secrets" {
-    for_each = var.vault_enabled ? [1] : []
-
-    content {
-      source_vault_id = var.source_vault_id
-
-      vault_certificates {
-        certificate_url   = var.certificate_url
-        certificate_store = var.certificate_store
-      }
-    }
-  }
-
-  dynamic "plan" {
-    for_each = var.plan_enabled ? [1] : []
-
-    content {
-      name      = var.plan_name
-      publisher = var.plan_publisher
-      product   = var.plan_product
-    }
-  }
-
-  storage_os_disk {
-    name                      = format("%s-storage-os-disk", module.labels.id)
-    create_option             = var.create_option
-    caching                   = var.caching
-    disk_size_gb              = var.disk_size_gb
-    image_uri                 = var.image_uri
-    os_type                   = var.os_type
-    write_accelerator_enabled = var.write_accelerator_enabled
-    managed_disk_id           = var.create_option == "Attach" ? var.managed_disk_id : null
-    managed_disk_type         = var.managed_disk_type
-    #vhd_uri                   = var.vhd_uri
-  }
-
-  dynamic "storage_data_disk" {
-    for_each = var.storage_data_disk_enabled ? [1] : []
-
-    content {
-      name                      = format("%s-storage-data-disk", module.labels.id)
-      create_option             = var.create_option
-      caching                   = var.caching
-      disk_size_gb              = var.disk_size_gb
-      lun                       = var.lun
-      write_accelerator_enabled = var.write_accelerator_enabled
-      managed_disk_id           = var.create_option == "Attach" ? var.managed_disk_id : ""
-      managed_disk_type         = var.ultra_ssd_enabled ? "UltraSSD_LRS" : var.managed_disk_type
-      #vhd_uri                   = var.vhd_uri
-    }
-  }
-
-  dynamic "storage_image_reference" {
-    for_each = var.storage_image_reference_enabled ? [1] : []
-
-    content {
-      publisher = var.custom_image_id == "" ? var.image_publisher : ""
-      offer     = var.custom_image_id == "" ? var.image_offer : ""
-      sku       = var.custom_image_id == "" ? var.image_sku : ""
-      version   = var.custom_image_id == "" ? var.image_version : ""
-      id        = var.custom_image_id
-    }
-  }
-
-  timeouts {
-    create = var.create
-    update = var.update
-    read   = var.read
-    delete = var.delete
-  }
-}
 
 #Module      : VIRTUAL MACHINE NETWORK SECURITY GROUP ASSOCIATION
 #Description : Terraform resource to create a virtual machine.
--- outputs.tf
+++ outputs.tf
@@ -23,30 +23,12 @@ output "public_ip_address" {
   description = "The IP address value that was allocated."
 }
 
-output "storage_account_id" {
-  value       = join("", azurerm_storage_account.default.*.id)
-  description = "The storage account Resource ID."
-}
-
-output "storage_account_primary_location" {
-  value       = join("", azurerm_storage_account.default.*.primary_location)
-  description = "The endpoint URL for blob storage in the primary location."
-}
-
-output "storage_account_primary_blob_endpoint" {
-  value       = join("", azurerm_storage_account.default.*.primary_blob_endpoint)
-  description = "The endpoint URL for blob storage in the primary location."
-}
 
 output "virtual_machine_id" {
   value       = join("", azurerm_virtual_machine.default.*.id)
   description = "The ID of the Virtual Machine."
 }
 
-# output "virtual_machine_identity" {
-#   value       = join("", azurerm_virtual_machine.default.identity.*.principal_id)
-#   description = "The Principal ID for the Service Principal associated with the Managed Service Identity of this Virtual Machine."
-# }
 
 output "network_interface_sg_association_id" {
   value       = join("", azurerm_network_interface_security_group_association.default.*.id)
