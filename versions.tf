a4d92eea97bbd37856bf945182c237eed64015da
mahesh yadav <mahesh.yadav@clouddrove.com>
2022-12-29 22:26:10 +0530

    update module

--- versions.tf
+++ versions.tf
@@ -1,4 +1,13 @@
 # Terraform version
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 1.0.0"
+}
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.0.0"
+    }
+  }
 }
