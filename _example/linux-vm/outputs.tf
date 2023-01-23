60ba9cd773b151534288505a7a1044ff14ceb986
nilesh.gadgi <nilesh.gadgi@clouddrove.com>
2023-01-24 00:08:26 +0530

    added the windows virtual machine module

--- _example/linux-vm/outputs.tf
+++ _example/linux-vm/outputs.tf
@@ -34,7 +34,7 @@ output "public_ip_address" {
 }
 
 output "virtual_machine_id" {
-  value       = module.virtual-machine.virtual_machine_id
+  value       = join("", module.virtual-machine.*.virtual_machine_id)
   description = "The ID of the Virtual Machine."
 }
 
